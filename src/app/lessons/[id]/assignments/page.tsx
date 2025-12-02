19353cb4355d8e0059198435abef0af993272504
khoabahoa <khoanmse183889@fpt.edu.vn>
2025-12-02 18:57:59 +0700

    Update page.tsx

--- src/app/lessons/[id]/assignments/page.tsx
+++ src/app/lessons/[id]/assignments/page.tsx
@@ -29,6 +29,11 @@ export default function AssignmentsPage() {
     QuestionType.SHORT_ANSWER,
   ]);
 
+  // Helper function to format question type
+  const formatQuestionType = (type: string): string => {
+    return type.replace("_", " ").replace(/\b\w/g, (l: string) => l.toUpperCase());
+  };
+
   // Load existing quizzes
   useEffect(() => {
     const loadQuizzes = async () => {
@@ -189,7 +194,7 @@ export default function AssignmentsPage() {
                         className="rounded border-orange-300"
                       />
                       <span className="text-sm text-gray-700 dark:text-gray-300">
-                        {type.replace("_", " ").replace(/\b\w/g, (l: string) => l.toUpperCase())}
+                        {formatQuestionType(type)}
                       </span>
                     </label>
                   ))}
